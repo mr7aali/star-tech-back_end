41b6e46a933a0e96a7a40eac75dddffd78eb04b3
Aali <mr7aali@gmail.com>
2025-07-11 12:37:45 +0600

    a

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -1,81 +1,70 @@
-import { CatagoryService } from './category.service';
+import { CatagoryService } from "./category.service";
 import { Request, Response } from "express";
 import catchAsync from "../../../shared/catchAsync";
-import sendResponse from '../../../shared/sendResponse';
-import { Category } from '@prisma/client';
+import sendResponse from "../../../shared/sendResponse";
+import { Category } from "@prisma/client";
 
-const create = catchAsync(
-    async (req: Request, res: Response) => {
-        const data = req.body;
-        const result = await CatagoryService.create(data);
+const create = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body;
+  const result = await CatagoryService.create(data);
 
+  sendResponse<Category>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Category created successfully!",
+    data: result,
+  });
+});
+const update = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body;
+  const id = req.params.id;
+  const result = await CatagoryService.update({ id, data });
 
-        sendResponse<Category>(res, {
-            statusCode: 200,
-            success: true,
-            message: "Category created successfully!",
-            data: result
-        })
-    }
-);
-const update = catchAsync(
-    async (req: Request, res: Response) => {
-        const data = req.body;
-        const id = req.params.id;
-        const result = await CatagoryService.update({ id, data });
+  sendResponse<Category>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Category created successfully!",
+    data: result,
+  });
+});
+const getAll = catchAsync(async (req: Request, res: Response) => {
+  const result = await CatagoryService.getAll();
 
+  sendResponse<Category[]>(res, {
+    statusCode: 200,
+    success: true,
+    message: "Category gotten successfully!",
+    data: result,
+  });
+});
+const getSingle = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
 
-        sendResponse<Category>(res, {
-            statusCode: 200,
-            success: true,
-            message: "Category created successfully!",
-            data: result
-        })
-    }
-);
-const getAll = catchAsync(
-    async (req: Request, res: Response) => {
+  const result = await CatagoryService.getSingle(id);
 
-        const result = await CatagoryService.getAll();
+  sendResponse<Category>(res, {
+    statusCode: 200,
+    success: true,
+    message: "single category!",
+    data: result,
+  });
+});
+const deleteCategory = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await CatagoryService.deleteCategory(id);
 
-
-        sendResponse<Category[]>(res, {
-            statusCode: 200,
-            success: true,
-            message: "Category gotten successfully!",
-            data: result
-        })
-    }
-);
-const getSingle = catchAsync(
-    async (req: Request, res: Response) => {
-        const id = req.params.id;
-        const result = await CatagoryService.getSingle(id);
-
-
-        sendResponse<Category>(res, {
-            statusCode: 200,
-            success: true,
-            message: "single category!",
-            data: result
-        })
-    }
-);
-const deleteCategory = catchAsync(
-    async (req: Request, res: Response) => {
-        const id = req.params.id;
-        const result = await CatagoryService.deleteCategory(id);
-
-
-        sendResponse<Category>(res, {
-            statusCode: 200,
-            success: true,
-            message: "single category!",
-            data: result
-        })
-    }
-);
+  sendResponse<Category>(res, {
+    statusCode: 200,
+    success: true,
+    message: "single category!",
+    data: result,
+  });
+});
 
 export const CategoryController = {
-    create, update, getAll, getSingle, deleteCategory
-}
+  create,
+  update,
+  getAll,
+  getSingle,
+  deleteCategory,
+};
--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,60 +1,70 @@
-import { Category } from "@prisma/client"
-import { prisma } from "../../../shared/prisma"
+import { Category } from "@prisma/client";
+import { prisma } from "../../../shared/prisma";
 import CustomError from "../../../errors/CustomError";
 import { StatusCodes } from "http-status-codes";
 
 const create = async (data: Category): Promise<Category> => {
-    const result = await prisma.category.create({ data });
-    if (!result) {
-        throw new CustomError(StatusCodes.BAD_REQUEST, "Category create failed!");
-    }
-    return result;
-}
-const update = async ({ id, data }: { id: string, data: Partial<Category>; }): Promise<Category> => {
-    const result = await prisma.category.update({
-        where: {
-            id: Number(id)
-        }, data
-    });
-    if (!result) {
-        throw new CustomError(StatusCodes.BAD_REQUEST, "Category Update failed!");
-    }
-    return result;
-}
+  const result = await prisma.category.create({ data });
+  if (!result) {
+    throw new CustomError(StatusCodes.BAD_REQUEST, "Category create failed!");
+  }
+  return result;
+};
+const update = async ({
+  id,
+  data,
+}: {
+  id: string;
+  data: Partial<Category>;
+}): Promise<Category> => {
+  const result = await prisma.category.update({
+    where: {
+      id: id,
+    },
+    data,
+  });
+  if (!result) {
+    throw new CustomError(StatusCodes.BAD_REQUEST, "Category Update failed!");
+  }
+  return result;
+};
 const getAll = async (): Promise<Category[]> => {
-
-    const result = await prisma.category.findMany({
-    });
-    if (!result) {
-        throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found!");
-    }
-    return result;
-}
+  const result = await prisma.category.findMany({});
+  if (!result) {
+    throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found!");
+  }
+  return result;
+};
 const getSingle = async (id: string): Promise<Category> => {
-    const result = await prisma.category.findUnique({
-        where: {
-            id: Number(id)
-        }, include: {
-            Product: true
-        }
-    })
-    if (!result) {
-        throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found!");
-    }
-    return result;
-}
+  const result = await prisma.category.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      products: true,
+    },
+  });
+  if (!result) {
+    throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found!");
+  }
+  return result;
+};
 const deleteCategory = async (id: string): Promise<Category> => {
-    const result = await prisma.category.delete({
-        where: {
-            id: Number(id)
-        }
-    })
-    if (!result) {
-        throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found");
-    }
-    return result;
-}
+  const result = await prisma.category.delete({
+    where: {
+      id: id,
+    },
+  });
+  if (!result) {
+    throw new CustomError(StatusCodes.BAD_REQUEST, "Category not found");
+  }
+  return result;
+};
 
 export const CatagoryService = {
-    create, update, getAll, getSingle, deleteCategory
-}
+  create,
+  update,
+  getAll,
+  getSingle,
+  deleteCategory,
+};
